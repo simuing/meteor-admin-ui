b9bf8ec151aa62ef049c91ac17f73c4ba54e6579
sz <wdd0988@gmail.com>
2021-04-27 19:30:04 +0900

    [add] Menu default data

--- imports/db/MenuCollection.ts
+++ imports/db/MenuCollection.ts
@@ -1,5 +1,26 @@
 import { Mongo } from 'meteor/mongo';
 
+/*
+    메뉴 코드 약어 설명
+    MN : Menu Code
+        &>00 : Menu Code Group
+        &>DB : Dashboard
+        &>MP : Mypage
+        &>GB : Guestbook
+            &>00 : Menu Code (00~99)
+ */
+
+const defaultMenu = {
+    menucd: 'MNDB00',
+    menuup: 'MN0000',
+    menunm:  'GuestBook',
+    url:  '/guestbook',
+    menulv:  '1',
+    menugb:  'G',
+    menuor:  '1',
+    createdAt: '20210427';
+}
+
 export interface Menu {
     _id?: string;
     menucd: string;
