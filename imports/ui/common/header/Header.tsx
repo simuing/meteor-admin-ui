a526d98a36cd38e8098aae9ecfcef0c6a80cbd45
simuing <wdd0988@gmail.com>
2021-05-02 23:18:26 +0900

    header el

--- imports/ui/common/header/Header.tsx
+++ imports/ui/common/header/Header.tsx
@@ -4,7 +4,9 @@ export const Header = () => {
 
   return (
     <header>
-      <div>안녕하세요 헤더입니다.</div>
+      <div className="sz-header-logo"></div>
+      <div className="sz-header-center"></div>
+      <div className="sz-header-right"></div>
     </header>
   );
 };
